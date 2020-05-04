95cbac38f5ee038d807a90eeac2dd4f8c9f08ef6
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-04 16:38:11 -0300

    add business project

---
+++ CineAPI.Business/Entities/FilmsBusiness.cs
@@ -0,0 +1,22 @@
+﻿using CineAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineAPI.Business.Entities
+{
+    public class FilmsBusiness
+    {
+        private AppDbContext context;
+
+        public FilmsBusiness(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Film>> GetFilms()
+            => await context.Films.ToListAsync();
+    }
+}

764fc4ff5fd7cc60c8099ad622516c4cb4793ae9
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-04 16:31:58 -0300

    add a connection context

---
+++ CineAPI.DAL/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CineAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Exhibition> Exhibitions { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Screen> Screens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}

cfc38ab809f7bdd91aaf1cfde36d68db09410a17
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-05 16:26:20 -0300

    adding new endpoints in Films

--- CineAPI/Controllers/FilmsController.cs
+++ CineAPI/Controllers/FilmsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CineAPI.Business.Entities;
 using CineAPI.Models;
+using CineAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,9 +25,47 @@ namespace CineAPI.Controllers
         /// Get all Films createds
         /// </summary>
         /// <returns>Return all Films</returns>
+        /// <response code="200">Success</response>
+        /// <response code="500">Internal Error</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
             => Ok(await business.GetAll());
+
+        /// <summary>
+        /// Get a Film by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Return a Film</returns>
+        /// <response code="200">Success</response>
+        /// <response code="500">Internal Error</response>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(int id)
+        {
+            Film film = await business.GetById(id);
+
+            if (film is null)
+                return NotFound();
+
+            return Ok(film);
+        }
+
+        /// <summary>
+        /// Get a Film by Name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Return a Film</returns>
+        /// <response code="200">Success</response>
+        /// <response code="500">Internal Error</response>
+        [HttpGet("name/{name}")]
+        public async Task<ActionResult<IEnumerable<ComboBoxViewModel>>> GetComboBoxName(string name)
+        {
+            var film = await business.GetComboBox(name);
+
+            if (film is null)
+                return NotFound();
+
+            return Ok(film);
+        }
 
         /// <summary>
         /// Create a Film

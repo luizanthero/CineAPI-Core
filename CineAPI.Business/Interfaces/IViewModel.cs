87e739d5ad7173e31cb324595ee544effeb241be
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-07 14:22:18 -0300

    adding interface for ViewModels

---
+++ CineAPI.Business/Interfaces/IViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CineAPI.Business.Interfaces
+{
+    public interface IViewModel<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllDetails();
+
+        Task<T> GetDetails(int id);
+    }
+}

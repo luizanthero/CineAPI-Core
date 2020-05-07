df9b2164cea790714677e8b82c10995b8daee6a1
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-07 14:27:05 -0300

    adding ViewModel details for Exhibition

---
+++ CineAPI.Domain/ViewModels/ExhibitionDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CineAPI.ViewModels
+{
+    public class ExhibitionDetailsViewModel
+    {
+        public int id { get; set; }
+
+        public string Filme { get; set; }
+
+        public string Room { get; set; }
+
+        public string Schedule { get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime updated_at { get; set; }
+    }
+}

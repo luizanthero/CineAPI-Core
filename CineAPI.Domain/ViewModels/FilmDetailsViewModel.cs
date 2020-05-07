f30f89c0b20b0a57d8e6f6c8267176406b9010b7
Luiz Anthero Gama <luizanthero@icloud.com>
2020-05-07 15:02:27 -0300

    adding Film Details ViewModel

---
+++ CineAPI.Domain/ViewModels/FilmDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CineAPI.ViewModels
+{
+    public class FilmDetailsViewModel
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ApiCode { get; set; }
+
+        public ICollection<ExhibitionDetailsViewModel> Exhibitions { get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime updated_at { get; set; }
+    }
+}

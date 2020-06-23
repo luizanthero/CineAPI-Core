59fa259ea8edf0331a1f29e35ff26a203562d661
Luiz Anthero Gama <luizanthero@icloud.com>
2020-06-22 23:44:01 -0300

    adding new columns on table films

--- CineAPI.Domain/Models/Film.cs
+++ CineAPI.Domain/Models/Film.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -18,6 +17,51 @@ namespace CineAPI.Models
 
         [Required]
         public string ApiCode { get; set; }
+
+        [Required]
+        public string Year { get; set; }
+
+        [Required]
+        public string Released { get; set; }
+
+        [Required]
+        public string Runtime { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Writer { get; set; }
+
+        [Required]
+        public string Actors { get; set; }
+
+        [Required]
+        public string Plot { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Awards { get; set; }
+
+        [Required]
+        public string Poster { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Production { get; set; }
+
+        [Required]
+        public string Website { get; set; }
 
         [DefaultValue(true)]
         [JsonIgnore]
